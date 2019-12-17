eb1e18124ec528fe63ba64d01eef4e0efc37ea73
William Wong <williamwdev@gmail.com>
2019-12-16 17:01:40 -0800

    added apollo-link-context and implemented authentication on client and server side

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./styles/index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
@@ -14,8 +16,18 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
