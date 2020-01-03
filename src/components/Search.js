2f4bfbd4c9e94c771a8819d94a885c8c04558b45
William Wong <williamwdev@gmail.com>
2020-01-02 20:36:37 -0800

    added FEED_SEARCH_QUERY to Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 import { withApollo } from "react-apollo";
+import gql from "graphql";
 // import gql from 'graphql-tag'
 import Link from "./Link";
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 class Search extends Component {
   state = {
